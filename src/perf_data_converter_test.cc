5109a7bd59c57a9978d55986a5a505f2d3721f86
aalexand <aalexand@google.com>
2020-06-29 12:09:23 -0700

    Use standard *.textproto suffix for the Quipper testdata files.
    PiperOrigin-RevId: 318864865

--- src/perf_data_converter_test.cc
+++ src/perf_data_converter_test.cc
@@ -250,7 +250,7 @@ TEST_F(PerfDataConverterTest, ConvertsGroupPid) {
 }
 
 TEST_F(PerfDataConverterTest, GroupByThreadTypes) {
-  string path(GetResource("single-event-multi-process-single-ip.pb_proto"));
+  string path(GetResource("single-event-multi-process-single-ip.textproto"));
   string ascii_pb = GetContents(path);
   ASSERT_FALSE(ascii_pb.empty()) << path;
   PerfDataProto perf_data_proto;
@@ -314,7 +314,7 @@ TEST_F(PerfDataConverterTest, Injects) {
 }
 
 TEST_F(PerfDataConverterTest, HandlesKernelMmapOverlappingUserCode) {
-  string path = GetResource("perf-overlapping-kernel-mapping.pb_proto");
+  string path = GetResource("perf-overlapping-kernel-mapping.textproto");
   string ascii_pb = GetContents(path);
   ASSERT_FALSE(ascii_pb.empty()) << path;
   PerfDataProto perf_data_proto;
@@ -337,7 +337,7 @@ TEST_F(PerfDataConverterTest, HandlesKernelMmapOverlappingUserCode) {
 }
 
 TEST_F(PerfDataConverterTest, HandlesCrOSKernel3_18Mapping) {
-  string path = GetResource("perf-cros-kernel-3_18-mapping.pb_proto");
+  string path = GetResource("perf-cros-kernel-3_18-mapping.textproto");
   string ascii_pb = GetContents(path);
   ASSERT_FALSE(ascii_pb.empty()) << path;
   PerfDataProto perf_data_proto;
@@ -360,7 +360,7 @@ TEST_F(PerfDataConverterTest, HandlesCrOSKernel3_18Mapping) {
 }
 
 TEST_F(PerfDataConverterTest, HandlesNonExecCommEvents) {
-  string path = GetResource("perf-non-exec-comm-events.pb_proto");
+  string path = GetResource("perf-non-exec-comm-events.textproto");
   string ascii_pb = GetContents(path);
   ASSERT_FALSE(ascii_pb.empty()) << path;
   PerfDataProto perf_data_proto;
@@ -381,7 +381,7 @@ TEST_F(PerfDataConverterTest, HandlesNonExecCommEvents) {
 }
 
 TEST_F(PerfDataConverterTest, HandlesIncludingCommMd5Prefix) {
-  string path = GetResource("perf-include-comm-md5-prefix.pb_proto");
+  string path = GetResource("perf-include-comm-md5-prefix.textproto");
   string ascii_pb = GetContents(path);
   ASSERT_FALSE(ascii_pb.empty()) << path;
   PerfDataProto perf_data_proto;
@@ -396,7 +396,7 @@ TEST_F(PerfDataConverterTest, HandlesIncludingCommMd5Prefix) {
 }
 
 TEST_F(PerfDataConverterTest, HandlesUnmappedCallchainIP) {
-  string path = GetResource("perf-unmapped-callchain-ip.pb_proto");
+  string path = GetResource("perf-unmapped-callchain-ip.textproto");
   string ascii_pb = GetContents(path);
   ASSERT_FALSE(ascii_pb.empty()) << path;
   PerfDataProto perf_data_proto;
@@ -466,7 +466,7 @@ TEST_F(PerfDataConverterTest, GroupsByComm) {
 }
 
 TEST_F(PerfDataConverterTest, SkipsFirstCallchainIPPebs) {
-  string ascii_pb(GetContents(GetResource("perf-callchain-pebs.pb_proto")));
+  string ascii_pb(GetContents(GetResource("perf-callchain-pebs.textproto")));
   ASSERT_FALSE(ascii_pb.empty());
   PerfDataProto perf_data_proto;
   ASSERT_TRUE(google::protobuf::TextFormat::ParseFromString(ascii_pb, &perf_data_proto));
@@ -481,7 +481,8 @@ TEST_F(PerfDataConverterTest, SkipsFirstCallchainIPPebs) {
 }
 
 TEST_F(PerfDataConverterTest, SkipsFirstCallchainIPNonPebs) {
-  string ascii_pb(GetContents(GetResource("perf-callchain-non-pebs.pb_proto")));
+  string ascii_pb(
+      GetContents(GetResource("perf-callchain-non-pebs.textproto")));
   ASSERT_FALSE(ascii_pb.empty());
   PerfDataProto perf_data_proto;
   ASSERT_TRUE(google::protobuf::TextFormat::ParseFromString(ascii_pb, &perf_data_proto));
