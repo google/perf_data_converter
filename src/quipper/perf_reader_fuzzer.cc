28eedf779a4d65960c2ccee385c2d72cf001e7a5
aalexand <aalexand@google.com>
2021-02-08 14:55:52 -0800

    Do not attempt to serialize the reader when the perf.data read failed.
    PiperOrigin-RevId: 356359812

--- src/quipper/perf_reader_fuzzer.cc
+++ src/quipper/perf_reader_fuzzer.cc
@@ -19,7 +19,12 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   const char *char_data = reinterpret_cast<const char *>(data);
 
   PerfReader pr;
-  pr.ReadFromPointer(char_data, size);
+  if (!pr.ReadFromPointer(char_data, size)) {
+    // Do not proceed to the serialization tests below if reading the file
+    // fails. The reader makes no guarantee about its state in case of failing
+    // to read the perf.data file so proceeding would be undefined behavior.
+    return 0;
+  }
 
   string raw_output;
   pr.WriteToString(&raw_output);
