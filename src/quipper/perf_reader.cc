5d90615e954735cd7452199fe09fa7c7c41ce969
skanev <skanev@google.com>
2022-03-03 10:27:24 -0800

    Preserve the full mapping name for the main kernel image.
    The mmap events for vmlinux include a DSO with a relocation symbol suffix (the "_stext" in
    "[kernel.kallsyms]_stext") that's necessary for correct symbolizaiton.
    
    They are differemt from the DSO names in build-id events (only "[kernel.kallsyms]").
    
    Instead of renaming the former to the latter (in order to match mmaps to build-ids),
    preserve the full mmap DSO names, and generate alternate build-id events with names
    that include the suffix.
    
    PiperOrigin-RevId: 432226042

--- src/quipper/perf_reader.cc
+++ src/quipper/perf_reader.cc
@@ -703,6 +703,20 @@ bool PerfReader::LocalizeUsingFilenames(
   return true;
 }
 
+bool PerfReader::AlternateBuildIDFilenames(
+    const std::unordered_multimap<std::string, std::string>& filenames) {
+  for (auto& build_id : *proto_->mutable_build_ids()) {
+    auto range = filenames.equal_range(build_id.filename());
+    for (auto it = range.first; it != range.second; it++) {
+      // Add a copy of this build id, with the alternate filename.
+      auto new_build_id = proto_->add_build_ids();
+      *new_build_id = build_id;
+      new_build_id->set_filename(it->second);
+    }
+  }
+  return true;
+}
+
 void PerfReader::GetFilenames(std::vector<string>* filenames) const {
   std::set<string> filename_set;
   GetFilenamesAsSet(&filename_set);
