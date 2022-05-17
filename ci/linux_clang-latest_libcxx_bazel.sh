b7dc2eadafe331844a8aac456c0ef91f49664b66
zmarcus <zmarcus@google.com>
2022-05-17 15:29:29 -0700

    Fix docker setup.
    PiperOrigin-RevId: 449330564

--- ci/linux_clang-latest_libcxx_bazel.sh
+++ ci/linux_clang-latest_libcxx_bazel.sh
@@ -15,41 +15,41 @@ if [ -z "${STD:-}" ]; then
   STD="c++17"
 fi
 
-if [ -z "${COMPILATION_MODE:-}" ]; then
-  COMPILATION_MODE="fastbuild opt"
-fi
+# This container is a reasonable start but is missing some prerequisite libs and
+# an installation of linux_perf.
+readonly DOCKER_CONTAINER="gcr.io/google.com/absl-177019/linux_hybrid-latest:20210617"
 
-if [ -z "${EXCEPTIONS_MODE:-}" ]; then
-  EXCEPTIONS_MODE="-fno-exceptions -fexceptions"
-fi
-
-readonly DOCKER_CONTAINER="gcr.io/google.com/absl-177019/linux_hybrid-latest:20210525"
-
-for std in ${STD}; do
-  for compilation_mode in ${COMPILATION_MODE}; do
-    for exceptions_mode in ${EXCEPTIONS_MODE}; do
-      echo "--------------------------------------------------------------------"
-      time docker run \
-        --volume="${PDC_ROOT}:/perf_data_converter:ro" \
-        --workdir=/perf_data_converter\
-        --cap-add=SYS_PTRACE \
-        --rm \
-        -e CC="/opt/llvm/clang/bin/clang" \
-        -e BAZEL_CXXOPTS="-std=${std}:-nostdinc++" \
-        -e BAZEL_LINKOPTS="-L/opt/llvm/libcxx/lib:-lc++:-lc++abi:-lm:-Wl,-rpath=/opt/llvm/libcxx/lib" \
-        -e CPLUS_INCLUDE_PATH="/opt/llvm/libcxx/include/c++/v1" \
-        ${DOCKER_EXTRA_ARGS:-} \
-        ${DOCKER_CONTAINER} \
-        /usr/local/bin/bazel test ... \
-          --compilation_mode="${compilation_mode}" \
-          --copt="${exceptions_mode}" \
-          --copt=-Werror \
-          --distdir="/bazel-distdir" \
-          --keep_going \
-          --show_timestamps \
-          --test_env="GTEST_INSTALL_FAILURE_SIGNAL_HANDLER=1" \
-          --test_output=errors \
-          ${BAZEL_EXTRA_ARGS:-}
-    done
-  done
-done
+time docker run \
+    --volume="${PDC_ROOT}:/perf_data_converter:ro" \
+    --workdir=/perf_data_converter \
+    --cap-add=SYS_PTRACE \
+    --rm \
+    -i \
+    -e CC="/opt/llvm/clang/bin/clang" \
+    -e BAZEL_CXXOPTS="-std=${STD}:-nostdinc++" \
+    -e BAZEL_LINKOPTS="-L/opt/llvm/libcxx/lib:-lc++:-lc++abi:-lm:-Wl,-rpath=/opt/llvm/libcxx/lib" \
+    -e CPLUS_INCLUDE_PATH="/opt/llvm/libcxx/include/c++/v1" \
+    ${DOCKER_EXTRA_ARGS:-} \
+    ${DOCKER_CONTAINER} \
+    bash <<EOF
+      apt-get -y update
+      apt-get -y install linux-perf
+      # /usr/bin/perf has heuristics that reason perf_5.0 exists in this image.
+      ln -s /usr/bin/perf_5.10 /usr/bin/perf_5.0
+      rm -rf ~/.cache/bazel
+      apt-get -y install g++ git libelf-dev libcap-dev
+      git submodule init
+      git submodule update
+      /usr/local/bin/bazel build src:all
+      /usr/local/bin/bazel build src/quipper:all
+      /usr/local/bin/bazel test ... \
+        --compilation_mode=fastbuild \
+        --copt=-fexceptions \
+        --copt=-Werror \
+        --distdir="/bazel-distdir" \
+        --keep_going \
+        --show_timestamps \
+        --test_env="GTEST_INSTALL_FAILURE_SIGNAL_HANDLER=1" \
+        --test_output=errors \
+        ${BAZEL_EXTRA_ARGS:-}
+EOF
