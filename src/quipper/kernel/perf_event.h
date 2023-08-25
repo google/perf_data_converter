72e07249d383c7debabebc0856d5dab06e94b524
xliuprof <xliuprof@google.com>
2023-08-25 14:32:01 -0700

    Add data source label support
    PiperOrigin-RevId: 560196881

--- src/quipper/kernel/perf_event.h
+++ src/quipper/kernel/perf_event.h
@@ -1040,59 +1040,48 @@ union perf_mem_data_src {
   };
 };
 
-/*
-// The below macros are not used in quipper. They are commented out for future
-// reference.
 // type of opcode (load/store/prefetch,code)
-#define PERF_MEM_OP_NA 0x01     // not available
-#define PERF_MEM_OP_LOAD 0x02   // load instruction
-#define PERF_MEM_OP_STORE 0x04  // store instruction
-#define PERF_MEM_OP_PFETCH 0x08 // prefetch
-#define PERF_MEM_OP_EXEC 0x10   // code (execution)
-#define PERF_MEM_OP_SHIFT 0
+const u64 PERF_MEM_OP_NA = 0x01;      // not available
+const u64 PERF_MEM_OP_LOAD = 0x02;    // load instruction
+const u64 PERF_MEM_OP_STORE = 0x04;   // store instruction
+const u64 PERF_MEM_OP_PFETCH = 0x08;  // prefetch
+const u64 PERF_MEM_OP_EXEC = 0x10;    // code (execution)
 
 // memory hierarchy (memory level, hit or miss)
-#define PERF_MEM_LVL_NA 0x01        // not available
-#define PERF_MEM_LVL_HIT 0x02       // hit level
-#define PERF_MEM_LVL_MISS 0x04      // miss level
-#define PERF_MEM_LVL_L1 0x08        // L1
-#define PERF_MEM_LVL_LFB 0x10       // Line Fill Buffer
-#define PERF_MEM_LVL_L2 0x20        // L2
-#define PERF_MEM_LVL_L3 0x40        // L3
-#define PERF_MEM_LVL_LOC_RAM 0x80   // Local DRAM
-#define PERF_MEM_LVL_REM_RAM1 0x100 // Remote DRAM (1 hop)
-#define PERF_MEM_LVL_REM_RAM2 0x200 // Remote DRAM (2 hops)
-#define PERF_MEM_LVL_REM_CCE1 0x400 // Remote Cache (1 hop)
-#define PERF_MEM_LVL_REM_CCE2 0x800 // Remote Cache (2 hops)
-#define PERF_MEM_LVL_IO 0x1000      // I/O memory
-#define PERF_MEM_LVL_UNC 0x2000     // Uncached memory
-#define PERF_MEM_LVL_SHIFT 5
+const u64 PERF_MEM_LVL_NA = 0x01;         // not available
+const u64 PERF_MEM_LVL_HIT = 0x02;        // hit level
+const u64 PERF_MEM_LVL_MISS = 0x04;       // miss level
+const u64 PERF_MEM_LVL_L1 = 0x08;         // L1
+const u64 PERF_MEM_LVL_LFB = 0x10;        // Line Fill Buffer
+const u64 PERF_MEM_LVL_L2 = 0x20;         // L2
+const u64 PERF_MEM_LVL_L3 = 0x40;         // L3
+const u64 PERF_MEM_LVL_LOC_RAM = 0x80;    // Local DRAM
+const u64 PERF_MEM_LVL_REM_RAM1 = 0x100;  // Remote DRAM (1 hop)
+const u64 PERF_MEM_LVL_REM_RAM2 = 0x200;  // Remote DRAM (2 hops)
+const u64 PERF_MEM_LVL_REM_CCE1 = 0x400;  // Remote Cache (1 hop)
+const u64 PERF_MEM_LVL_REM_CCE2 = 0x800;  // Remote Cache (2 hops)
+const u64 PERF_MEM_LVL_IO = 0x1000;       // I/O memory
+const u64 PERF_MEM_LVL_UNC = 0x2000;      // Uncached memory
 
 // snoop mode
-#define PERF_MEM_SNOOP_NA 0x01   // not available
-#define PERF_MEM_SNOOP_NONE 0x02 // no snoop
-#define PERF_MEM_SNOOP_HIT 0x04  // snoop hit
-#define PERF_MEM_SNOOP_MISS 0x08 // snoop miss
-#define PERF_MEM_SNOOP_HITM 0x10 // snoop hit modified
-#define PERF_MEM_SNOOP_SHIFT 19
+const u64 PERF_MEM_SNOOP_NA = 0x01;    // not available
+const u64 PERF_MEM_SNOOP_NONE = 0x02;  // no snoop
+const u64 PERF_MEM_SNOOP_HIT = 0x04;   // snoop hit
+const u64 PERF_MEM_SNOOP_MISS = 0x08;  // snoop miss
+const u64 PERF_MEM_SNOOP_HITM = 0x10;  // snoop hit modified
 
 // locked instruction
-#define PERF_MEM_LOCK_NA 0x01     // not available
-#define PERF_MEM_LOCK_LOCKED 0x02 // locked transaction
-#define PERF_MEM_LOCK_SHIFT 24
+const u64 PERF_MEM_LOCK_NA = 0x01;      // not available
+const u64 PERF_MEM_LOCK_LOCKED = 0x02;  // locked transaction
 
 // TLB access
-#define PERF_MEM_TLB_NA 0x01   // not available
-#define PERF_MEM_TLB_HIT 0x02  // hit level
-#define PERF_MEM_TLB_MISS 0x04 // miss level
-#define PERF_MEM_TLB_L1 0x08   // L1
-#define PERF_MEM_TLB_L2 0x10   // L2
-#define PERF_MEM_TLB_WK 0x20   // Hardware Walker
-#define PERF_MEM_TLB_OS 0x40   // OS fault handler
-#define PERF_MEM_TLB_SHIFT 26
-
-#define PERF_MEM_S(a, s) (((__u64)PERF_MEM_##a##_##s) << PERF_MEM_##a##_SHIFT)
-*/
+const u64 PERF_MEM_TLB_NA = 0x01;    // not available
+const u64 PERF_MEM_TLB_HIT = 0x02;   // hit level
+const u64 PERF_MEM_TLB_MISS = 0x04;  // miss level
+const u64 PERF_MEM_TLB_L1 = 0x08;    // L1
+const u64 PERF_MEM_TLB_L2 = 0x10;    // L2
+const u64 PERF_MEM_TLB_WK = 0x20;    // Hardware Walker
+const u64 PERF_MEM_TLB_OS = 0x40;    // OS fault handler
 
 /*
  * single taken branch record layout:
