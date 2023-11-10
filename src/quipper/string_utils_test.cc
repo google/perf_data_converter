7160ed50b0effd6dc9823712b14a3eed45d8bfae
gmx <gmx@google.com>
2023-11-09 21:33:51 -0800

    Move RootPath to string_utils and make it public so it can be tested.
    PiperOrigin-RevId: 581118169

---
+++ src/quipper/string_utils_test.cc
@@ -0,0 +1,23 @@
+// Copyright (c) 2023 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "string_utils.h"
+
+#include "compat/test.h"
+
+namespace quipper {
+
+TEST(StringUtilsTest, RootPathExtraction) {
+  EXPECT_EQ(RootPath("/aa"), "");
+  EXPECT_EQ(RootPath("//aa"), "/");
+  EXPECT_EQ(RootPath("/aa/bb"), "/aa");
+  EXPECT_EQ(RootPath("//aa//bb"), "//aa/");
+  EXPECT_EQ(RootPath("/aa/bb/cc"), "/aa/bb");
+  EXPECT_EQ(RootPath("//aa//bb//cc"), "//aa//bb/");
+  EXPECT_EQ(RootPath("/aa/bb/cc/dd"), "/aa/bb");
+  EXPECT_EQ(RootPath("//aa//bb//cc//dd"), "//aa//bb/");
+  EXPECT_EQ(RootPath("[vdso]"), "");
+}
+
+}  // namespace quipper
