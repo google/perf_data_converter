df0422804fc06863ca3f04b7d31b551384bb7e7a
gmx <gmx@google.com>
2023-11-10 21:52:45 -0800

    Fix corner cases in root path extraction and generate custom root paths for named VMAs.
    PiperOrigin-RevId: 581450599

--- src/quipper/string_utils_test.cc
+++ src/quipper/string_utils_test.cc
@@ -9,15 +9,17 @@
 namespace quipper {
 
 TEST(StringUtilsTest, RootPathExtraction) {
-  EXPECT_EQ(RootPath("/aa"), "");
-  EXPECT_EQ(RootPath("//aa"), "/");
+  EXPECT_EQ(RootPath("/aa"), "/");
+  EXPECT_EQ(RootPath("//aa"), "//");
   EXPECT_EQ(RootPath("/aa/bb"), "/aa");
-  EXPECT_EQ(RootPath("//aa//bb"), "//aa/");
+  EXPECT_EQ(RootPath("//aa//bb"), "//aa");
   EXPECT_EQ(RootPath("/aa/bb/cc"), "/aa/bb");
-  EXPECT_EQ(RootPath("//aa//bb//cc"), "//aa//bb/");
+  EXPECT_EQ(RootPath("//aa//bb//cc"), "//aa//bb");
   EXPECT_EQ(RootPath("/aa/bb/cc/dd"), "/aa/bb");
-  EXPECT_EQ(RootPath("//aa//bb//cc//dd"), "//aa//bb/");
+  EXPECT_EQ(RootPath("//aa//bb//cc//dd"), "//aa//bb");
   EXPECT_EQ(RootPath("[vdso]"), "");
+  EXPECT_EQ(RootPath("[anon:Mem0x20000]"), "[anon:");
+  EXPECT_EQ(RootPath("/memfd:temp_file (deleted)"), "/memfd:");
 }
 
 }  // namespace quipper
