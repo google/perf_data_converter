d99e5cea76354536f364277d23a1fd8230eb3eef
gmx <gmx@google.com>
2026-06-04 20:51:01 +0000

    Include the correct header for the CHECK() macro.
    ChromeOS defines it in "base/check.h" while other systems define it in
    "base/logging.h".
    
    PiperOrigin-RevId: 926868910

--- src/quipper/address_mapper.h
+++ src/quipper/address_mapper.h
@@ -11,7 +11,7 @@
 #include <list>
 #include <map>
 
-#include "base/logging.h"
+#include "compat/check.h"
 
 namespace quipper {
 
---
+++ src/quipper/compat/check.h
@@ -0,0 +1,10 @@
+// Copyright 2026 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMIUMOS_WIDE_PROFILING_COMPAT_CHECK_H_
+#define CHROMIUMOS_WIDE_PROFILING_COMPAT_CHECK_H_
+
+#include "detail/check.h"
+
+#endif  // CHROMIUMOS_WIDE_PROFILING_COMPAT_CHECK_H_
